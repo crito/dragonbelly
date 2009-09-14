d6ec3fb7b33cf2c5d92b6fca2ac971656257e46d
crito <crito@fenix.amstel.squat>
2009-09-14 21:00:28 +0200

    Removed the camel case notation from the threadpool.py.

--- lib/threadpool.py
+++ lib/threadpool.py
@@ -1,160 +1,140 @@
 import threading
 from time import sleep
 
-class ThreadPool:
-
-    """Flexible thread pool class.  Creates a pool of threads, then
+class ThreadPool(object):
+    """
+    Flexible thread pool class.  Creates a pool of threads, then
     accepts tasks that will be dispatched to the next available
-    thread."""
-    
-    def __init__(self, numThreads):
+    thread.
+    """
+    def __init__(self, num_threads):
+        """Initialize the thread pool with num_threads workers."""
+        self._threads = []
+        self._resize_lock = threading.Condition(threading.Lock())
+        self._task_lock = threading.Condition(threading.Lock())
+        self._tasks = []
+        self._is_joining = False
+        self.set_thread_count(num_threads)
 
-        """Initialize the thread pool with numThreads workers."""
-        
-        self.__threads = []
-        self.__resizeLock = threading.Condition(threading.Lock())
-        self.__taskLock = threading.Condition(threading.Lock())
-        self.__tasks = []
-        self.__isJoining = False
-        self.setThreadCount(numThreads)
-
-    def setThreadCount(self, newNumThreads):
-
+    def set_thread_count(self, new_num_threads):
         """ External method to set the current pool size.  Acquires
         the resizing lock, then calls the internal version to do real
         work."""
-        
         # Can't change the thread count if we're shutting down the pool!
-        if self.__isJoining:
+        if self._is_joining:
             return False
-        
-        self.__resizeLock.acquire()
+
+        self._resize_lock.acquire()
         try:
-            self.__setThreadCountNolock(newNumThreads)
+            self._set_thread_count_no_lock(new_num_threads)
         finally:
-            self.__resizeLock.release()
+            self._resize_lock.release()
         return True
 
-    def __setThreadCountNolock(self, newNumThreads):
-        
+    def _set_thread_count_no_lock(self, new_num_threads):
         """Set the current pool size, spawning or terminating threads
-        if necessary.  Internal use only; assumes the resizing lock is
+        if necessary. Internal use only; assumes the resizing lock is
         held."""
-        
         # If we need to grow the pool, do so
-        while newNumThreads > len(self.__threads):
-            newThread = ThreadPoolThread(self)
-            self.__threads.append(newThread)
-            newThread.start()
+        while new_num_threads > len(self._threads):
+            new_thread = ThreadPoolThread(self)
+            self._threads.append(new_thread)
+            new_thread.start()
         # If we need to shrink the pool, do so
-        while newNumThreads < len(self.__threads):
-            self.__threads[0].goAway()
-            del self.__threads[0]
+        while new_num_threads < len(self._threads):
+            self._threads[0].go_away()
+            del self._threads[0]
 
-    def getThreadCount(self):
-
+    def get_thread_count(self):
         """Return the number of threads in the pool."""
-        
-        self.__resizeLock.acquire()
+        self._resize_lock.acquire()
         try:
-            return len(self.__threads)
+            return len(self._threads)
         finally:
-            self.__resizeLock.release()
+            self._resize_lock.release()
 
-    def queueTask(self, task, args=None, taskCallback=None):
-
-        """Insert a task into the queue.  task must be callable;
-        args and taskCallback can be None."""
-        
-        if self.__isJoining == True:
+    def queue_task(self, task, args=None, task_callback=None):
+        """Insert a task into the queue. task must be callable;
+        args and task_callback can be None."""
+        if self._is_joining == True:
             return False
         if not callable(task):
             return False
-        
-        self.__taskLock.acquire()
+
+        self._task_lock.acquire()
         try:
-            self.__tasks.append((task, args, taskCallback))
+            self._tasks.append((task, args, task_callback))
             return True
         finally:
-            self.__taskLock.release()
+            self._task_lock.release()
 
-    def getNextTask(self):
-
-        """ Retrieve the next task from the task queue.  For use
+    def get_next_task(self):
+        """Retrieve the next task from the task queue. For use
         only by ThreadPoolThread objects contained in the pool."""
-        
-        self.__taskLock.acquire()
+        self._task_lock.acquire()
         try:
-            if self.__tasks == []:
+            if self._tasks == []:
                 return (None, None, None)
             else:
-                return self.__tasks.pop(0)
+                return self._tasks.pop(0)
         finally:
-            self.__taskLock.release()
-    
-    def joinAll(self, waitForTasks = True, waitForThreads = True):
+            self._task_lock.release()
 
-        """ Clear the task queue and terminate all pooled threads,
+    def join_all(self, wait_for_tasks = True, wait_for_threads = True):
+        """Clear the task queue and terminate all pooled threads,
         optionally allowing the tasks and threads to finish."""
-        
         # Mark the pool as joining to prevent any more task queueing
-        self.__isJoining = True
+        self._is_joining = True
 
         # Wait for tasks to finish
-        if waitForTasks:
-            while self.__tasks != []:
+        if wait_for_tasks:
+            while self._tasks != []:
                 sleep(.1)
 
         # Tell all the threads to quit
-        self.__resizeLock.acquire()
+        self._resize_lock.acquire()
         try:
-            self.__setThreadCountNolock(0)
-            self.__isJoining = True
+            self._set_thread_count_no_lock(0)
+            self._is_joining = True
 
             # Wait until all threads have exited
-            if waitForThreads:
-                for t in self.__threads:
+            if wait_for_threads:
+                for t in self._threads:
                     t.join()
                     del t
 
             # Reset the pool for potential reuse
-            self.__isJoining = False
+            self._is_joining = False
         finally:
-            self.__resizeLock.release()
+            self._resize_lock.release()
 
-
-        
 class ThreadPoolThread(threading.Thread):
-
-    """ Pooled thread class. """
-    
-    threadSleepTime = 0.1
+    """
+    Pooled thread class.
+    """
+    thread_sleep_time = 0.1
 
     def __init__(self, pool):
-
-        """ Initialize the thread and remember the pool. """
-        
+        """Initialize the thread and remember the pool."""
         threading.Thread.__init__(self)
-        self.__pool = pool
-        self.__isDying = False
-        
-    def run(self):
+        self._pool = pool
+        self._is_dying = False
 
-        """ Until told to quit, retrieve the next task and execute
-        it, calling the callback if any.  """
-        
-        while self.__isDying == False:
-            cmd, args, callback = self.__pool.getNextTask()
+    def run(self):
+        """Until told to quit, retrieve the next task and execute
+        it, calling the callback if any."""
+        while self._is_dying == False:
+            cmd, args, callback = self._pool.get_next_task()
             # If there's nothing to do, just sleep a bit
             if cmd is None:
-                sleep(ThreadPoolThread.threadSleepTime)
-            elif callback is None:
+                sleep(ThreadPoolThread.thread_sleep_time)
+            elif args is None:
                 cmd(args)
+            elif callback is None:
+                cmd()
             else:
                 callback(cmd(args))
-    
-    def goAway(self):
 
-        """ Exit the run loop next time through."""
-        
+    def go_away(self):
+        """Exit the run loop next time through."""
         self.__isDying = True
