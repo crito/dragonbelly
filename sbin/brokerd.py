8be32c95b8692ff67554a60b228b045bd810093b
crito <crito@fenix.amstel.squat>
2009-09-14 12:28:32 +0200

    Replaced EventQueue with a PriorityQueue from the python cookbook.

--- sbin/brokerd.py
+++ sbin/brokerd.py
@@ -4,9 +4,10 @@ brokerd - Manage incoming HTTP requests and redirect these requests
           the physical URL available.
 """
 
+import sys
 import logging
 import logging.handlers
-import queue
+import queue, heapq, time
 from http.server import HTTPServer, BaseHTTPRequestHandler
 
 __version__  = '0.1'
@@ -18,32 +19,76 @@ log.setLevel(logging.DEBUG)
 log_handler = logging.FileHandler(logfile)
 log.addHandler(log_handler)
 
-
 class Event(object):
     def __init__(self):
         self.data = {}
 
     def add(self, key, value):
-        log.debug('Event added: %s => %s' % (key, value))
+        log.debug('Event key/value added: %s => %s' % (key, value))
         self.data[key] = value
 
     def get(self):
-        log.debug('Event retrieved: %s => %s' % (key, value))
+        log.debug('Event key/value retrieved: %s => %s' % (key, value))
         return self.request
 
-class EventQueue(object):
-    def __init__(self):
-        self.queue = queue.Queue(1024)
+    def add_to_queue():
+        pass
 
-    def put(self, item):
-        self.queue.put_nowait(item)
+class EventQueue(queue.Queue):
+    """
+    FIFO Queue qith Priority handling.
+    See Python Cookbook, chapter 9, recipe 3 (Priority Queue).
+    """
+    def _init(self, maxsize):
+        """Initializes the array to use for the eventQueue. Called by 
+        __init__() of the parent class."""
+        self.maxsize = maxsize
+        self.queue = [ ]
 
-    def get(self):
-        item = self.queue.get()
+    def _qsize(self):
+        """Return current amount of elements of the EventQueue."""
+        return len(self.queue)
+
+    def _empty(self):
+        """Check whether the EventQueue is empty."""
+        return not self.queue
+
+    def _full(self):
+        """Check whether EventQueue is full."""
+        return self.maxsize > 0 and len(self.queue) >= self.maxsize
+
+    def _put(self, item):
+        """Put a new item onto the queue."""
+        heapq.heappush(self.queue, item)
+
+    def _get(self):
+        """Retrieve next item from the queue."""
+        return heapq.heappop(self.queue)
+
+    def put(self, item, priority=0, block=True, timeout=None):
+        """Override Queue.Queue's put function to incorporate the priority
+        argument."""
+        decorated_item = priority, time.time(), item
+        queue.Queue.put(self, decorated_item, block, timeout)
+
+    def get(self, block=True, timeout=None):
+        """Override Queue.Queue's get function."""
+        priority, time_posted, item = queue.Queue.get(self, block, timeout)
         return item
 
-    def size(self):
-        return self.queue.qsize()
+#class EventQueue(object):
+#    def __init__(self):
+#        self.queue = queue.Queue(1024)
+#
+#    def put(self, item):
+#        self.queue.put_nowait(item)
+#
+#    def get(self):
+#        item = self.queue.get()
+#        return item
+#
+#    def size(self):
+#        return self.queue.qsize()
 
 class RequestHandler(BaseHTTPRequestHandler):
     def do_GET(self):
@@ -51,16 +96,14 @@ class RequestHandler(BaseHTTPRequestHandler):
         e.add('method', 'GET')
         e.add('path', self.path)
         e.add('http_version', self.request_version)
-        q.put(e)
-        q.size()
+        log.debug("Adding event")
 
     def do_PUT(self):
         e = Event()
-        e.add('method', 'GET')
+        e.add('method', 'PUT')
         e.add('path', self.path)
         e.add('http_version', self.request_version)
-        q.put(e)
-        q.size()
+        log.debug("Adding event")
 
 class HttpListener(object):
     def __init__(self, host, port):
@@ -74,10 +117,6 @@ class Dispatcher(object):
     def __init__(self):
         self.event_handlers = []
 
-
-q = EventQueue()
 if __name__ == "__main__":
-    print("Queue Size: %d" % q.size())
     h = HttpListener('localhost', 8000)
     h.run()
-
